8c078d631ac22696c78796bfa34c4da0d34868e1
Rammun <russslllan@mail.ru>
2016-03-30 02:31:28 +0300

    Добавил классы User, Battle, Game и тесты к ним

---
+++ GameLogic/Battle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    public class Battle
+    {
+        User user1;
+        User user2;
+
+        public Battle(User user1, User user2)
+        {
+            this.user1 = user1;
+            this.user2 = user2;
+        }
+
+        public User User1 { get { return user1; } }
+        public User User2 { get { return user2; } }
+    }
+}
---
+++ GameLogic/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    public enum Race { Human, Magic }
+
+    public class User
+    {
+        string name;
+        Race race;
+
+        public User(string name, Race race)
+        {
+            this.name = name;
+            this.race = race;
+        }
+        public string Name { get { return name; } }
+        public Race Race { get { return race; } }
+    }
+}
